2bb09a9ece31f99d20ef471acfa754c8f4518479
Akira Hayashi <rindrics@gmail.com>
2026-06-11 02:39:17 +0900

    build: generate API schema

--- adapters/legacy/R/main.R
+++ adapters/legacy/R/main.R
@@ -1,7 +1,10 @@
 start_server <- function() {
     pr <- plumber::pr()
     pr$mount("/vpa", plumber::pr("R/vpa.R"))
-    
+
+    spec <- pr$getApiSpec()
+    jsonlite::write_json(spec, "../../schema/openapi.json", pretty = TRUE)
+
     port <- Sys.getenv("PLUMBER_PORT")
     pr$run(port = port)
 }
